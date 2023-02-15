87da8ed8867a2c21f78e5be045ac77376f39da52
ivan valle <54561077+Ivanvallesj@users.noreply.github.com>
2023-02-15 18:10:40 -0300

    Media queris
    Configurando el modo mobile

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,32 @@
 const btnEnviarEmail = document.querySelector('#btnEnviarEmail')
 
+const openMenu = document.querySelector('#open-menu')
+const closeMenu = document.querySelector('#close-menu')
+const navMenu = document.querySelector('#nav-list')
+const ocultarWorks = document.querySelector('#works')
+const navLinks = document.querySelectorAll('.nav-link')
+
+
+openMenu.addEventListener('click', () =>{
+  navMenu.classList.add('active')
+  ocultarWorks.classList.add('ocultar')
+})
+
+closeMenu.addEventListener('click', () =>{
+  navMenu.classList.remove('active')
+  ocultarWorks.classList.remove('ocultar')
+})
+
+console.log(navLinks)
+
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', ()=>{
+      navMenu.classList.remove('active')
+      ocultarWorks.classList.remove('ocultar')
+    })
+});
+
+
 
 btnEnviarEmail.addEventListener('click',() =>{
     Swal.fire({
